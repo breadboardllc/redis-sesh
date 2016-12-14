e6e506f7e8c01ba95e92df0d5ecf3685ffa2ebcc
David Murdoch <david@davidmurdoch.com>
2016-12-14 14:31:27 -0500

    fix ttl not defined error

--- redis-sesh.js
+++ redis-sesh.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var easyPbkdf2 = require("easy-pbkdf2")();
+const easyPbkdf2 = require("easy-pbkdf2")();
 
 function RedisSession( redisClient, sessionPrefix, ttl ) {
 	this._client = redisClient;
@@ -10,11 +10,12 @@ function RedisSession( redisClient, sessionPrefix, ttl ) {
 
 RedisSession.prototype = {
 	"set": function(userId, callback){
+		const ttl = this._ttl;
 		return easyPbkdf2.random( 21, function( buf ) {
 			const sessionId = buf.toString( "hex" );
 			const args = [this._prefix + sessionId, userId];
 			if ( ttl ) {
-				args.push("EX", this.ttl);
+				args.push("EX", ttl);
 			}
 			this._client.set( args, function( err ) {
 				callback(err, err ? undefined : sessionId);
@@ -28,7 +29,7 @@ RedisSession.prototype = {
 		this._client.del(this._prefix + sessionId, callback);
 	},
 	"liv": function(sessionId, callback){
-		var ttl = this.ttl;
+		const ttl = this._ttl;
 		if ( ttl ) return callback();
 		this._client.expire(this._prefix + sessionId, ttl, callback);
 	}
